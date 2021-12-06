be43c4dacb50229dd9b0733cefc11ca0dc76a412
TonyJBarr <tony@barr.contact>
2021-12-05 19:00:48 -0500

    wat

---
+++ main.py
@@ -0,0 +1,16 @@
+"""
+Extract platform names from file
+Extract game name's from file
+Add platform name to game name
+
+
+"""
+
+
+if __name__ == '__main__':
+    with open('game_list.txt') as file:
+        gamelst = {
+            game:publisher
+        }
+        if file.readline() == %2:
+            gamelst.append(game)
